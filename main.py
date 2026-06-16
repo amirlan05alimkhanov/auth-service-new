7e9c7e69125b812beb30270c59f72313b6dd26ee
Amirlan Alimkhanov <aamirlanalimkhanov05@gmail.com>
2026-06-16 10:02:22 +0500

    refactor: CORS fix

--- main.py
+++ main.py
@@ -1,20 +1,16 @@
 from contextlib import asynccontextmanager
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware  # <-- Проверь, есть ли этот импорт!
+
 from database.connection import engine, Base
 from api.routers.auth import router as auth_router
-from fastapi.middleware.cors import CORSMiddleware
-
-# Обязательно импортируем модели, чтобы Base.metadata о них узнал
 import models.user
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
-    # Действия при запуске (Создание таблиц)
     async with engine.begin() as conn:
-        await conn.run_sync(Base.metadata.drop_all)
         await conn.run_sync(Base.metadata.create_all)
     yield
-    # Действия при выключении
     await engine.dispose()
 
 app = FastAPI(
@@ -22,13 +18,13 @@ app = FastAPI(
     lifespan=lifespan
 )
 
-# Настройка CORS
+# Настройка CORS должна быть прямо здесь
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"], # Во время разработки разрешаем запросы отовсюду
+    allow_origins=["*"],
     allow_credentials=True,
-    allow_methods=["*"], # Разрешаем все методы (POST, GET и т.д.)
-    allow_headers=["*"], # Разрешаем все заголовки (включая токены Authorization)
+    allow_methods=["*"],
+    allow_headers=["*"],
 )
 
 app.include_router(auth_router)
